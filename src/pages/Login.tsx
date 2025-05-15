c9ac0117daa4a992ae208f1c6f317ba37cec2c98
Yahya Banouk <yahya.banouk@hotmail.com>
2025-05-15 20:07:54 +0200

    login form with antd

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,21 +1,51 @@
+import React from 'react';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { login } from '../redux/authSlice';
 import type { AppDispatch } from '../redux/store';
+import { Form, Input, Button, Card } from 'antd';
 
 const Login = () => {
   const dispatch = useDispatch<AppDispatch>();
   const navigate = useNavigate();
 
-  const handleLogin = () => {
-    dispatch(login({ username: 'user' }));
+  const onFinish = (values: any) => {
+    dispatch(login({ username: values.username }));
     navigate('/');
   };
 
   return (
-    <div style={{ padding: '2rem' }}>
-      <h2>Login</h2>
-      <button onClick={handleLogin}>Log In</button>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <Card title="Login" style={{ width: 300 }}>
+        <Form
+          name="login"
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          layout="vertical"
+        >
+          <Form.Item
+            label="Username"
+            name="username"
+            rules={[{ required: true, message: 'Please input your username!' }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: 'Please input your password!' }]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit" block>
+              Log In
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
     </div>
   );
 };
