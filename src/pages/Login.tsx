40e76ca0f50417d15f2c1ee0e59b20e3049ab9f1
Yahya Banouk <yahya.banouk@hotmail.com>
2025-05-16 00:05:42 +0200

    formating

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -6,48 +6,48 @@ import type { AppDispatch } from '../redux/store';
 import { Form, Input, Button, Card } from 'antd';
 
 const Login = () => {
-  const dispatch = useDispatch<AppDispatch>();
-  const navigate = useNavigate();
+    const dispatch = useDispatch<AppDispatch>();
+    const navigate = useNavigate();
 
-  const onFinish = (values: any) => {
-    dispatch(login({ username: values.username }));
-    navigate('/');
-  };
+    const onFinish = (values: any) => {
+        dispatch(login({ username: values.username }));
+        navigate('/');
+    };
 
-  return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <Card title="Login" style={{ width: 300 }}>
-        <Form
-          name="login"
-          initialValues={{ remember: true }}
-          onFinish={onFinish}
-          layout="vertical"
-        >
-          <Form.Item
-            label="Username"
-            name="username"
-            rules={[{ required: true, message: 'Please input your username!' }]}
-          >
-            <Input />
-          </Form.Item>
+    return (
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+            <Card title="Login" style={{ width: 300 }}>
+                <Form
+                name="login"
+                initialValues={{ remember: true }}
+                onFinish={onFinish}
+                layout="vertical"
+                >
+                <Form.Item
+                    label="Username"
+                    name="username"
+                    rules={[{ required: true, message: 'Please input your username!' }]}
+                >
+                <Input />
+                </Form.Item>
 
-          <Form.Item
-            label="Password"
-            name="password"
-            rules={[{ required: true, message: 'Please input your password!' }]}
-          >
-            <Input.Password />
-          </Form.Item>
+                <Form.Item
+                    label="Password"
+                    name="password"
+                    rules={[{ required: true, message: 'Please input your password!' }]}
+                >
+                    <Input.Password />
+                </Form.Item>
 
-          <Form.Item>
-            <Button type="primary" htmlType="submit" block>
-              Log In
-            </Button>
-          </Form.Item>
-        </Form>
-      </Card>
+                <Form.Item>
+                    <Button type="primary" htmlType="submit" block>
+                        Log In
+                    </Button>
+                </Form.Item>
+            </Form>
+        </Card>
     </div>
-  );
+    );
 };
 
 export default Login;
